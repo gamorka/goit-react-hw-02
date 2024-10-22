b803c4f81764f58177a4195ef21285e2b35c0dc6
gamorka <gamorkaaa@gmail.com>
2024-10-22 20:30:55 +0200

    виправлення помилок

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import './App.css'
+import React, { useState } from 'react';
+
+function App() {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const updateFeedback = (type) => {
+    setFeedback((prevState) => ({
+      ...prevState,
+      [type]: prevState[type] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
+
+  return (
+    <div>
+      <h1>Sip Happens Café</h1>
+      <p>Please leave your feedback about our service by selecting one of the options below.</p>
+      
+      <Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} />
+      
+      <p>Good: {feedback.good}</p>
+      <p>Neutral: {feedback.neutral}</p>
+      <p>Bad: {feedback.bad}</p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positive: {positiveFeedback}%</p>
+    </div>
+  );
+}
+
+function Options({ updateFeedback, resetFeedback }) {
+  return (
+    <div>
+      <button onClick={() => updateFeedback('good')}>Good</button>
+      <button onClick={() => updateFeedback('neutral')}>Neutral</button>
+      <button onClick={() => updateFeedback('bad')}>Bad</button>
+      <button onClick={resetFeedback}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
